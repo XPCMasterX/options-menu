fe3404d445866fa73dce522b161a190cc11a500b
XPCMasterX <xpcmasterx007@gmail.com>
2021-03-01 11:51:52 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Options Component
+
+A Svelte Component for options. For use with Svelte only (as far as I know).
+
+# Documentation
+
+See [Documentation](docs.md)
+
+# Todo
+
+- [x] Write a skeleton test for Component
+- [ ] Finish Component
+  - [ ] Parse and create elements based on data
+  - [ ] Add support for input types.
+    - [ ] Slider
+    - [ ] Text Box
+    - [ ] Colour Picker
+  - [ ] Make Components Themable (instead of just editing the component itself)
+- [ ] Write Docs
+- [ ] Organize Component
---
+++ src/Component.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let data;
+</script>
+
+<div class="container">
+  <div class="options-menu">
+    <div class="option">
+      <input class="slider" type="range" min="0" max="100" />
+    </div>
+  </div>
+</div>
+
+<style>
+  :root {
+    --background-color: #fcfaf9;
+  }
+
+  .container {
+    /* Remove any white space around it */
+    margin-top: -1.1vh;
+  }
+
+  .options-menu {
+    background-color: var(--background-color);
+    color: white;
+    margin-left: -0.5vw;
+    width: 30vw;
+    height: 94.6vh;
+
+    padding: 20px;
+  }
+
+  .option {
+    border-radius: 10px;
+    background-color: #f3d3bd;
+    border: solid 2px white;
+    height: 5vh;
+  }
+</style>
