f15efea82265d1e2d815c5eee0b3e4eee434914c
XPCMasterX <xpcmasterx007@gmail.com>
2021-03-01 12:19:19 +0000

    Successfully parses and understands data + loads in css

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,40 @@
 <script>
+  import { onMount } from "svelte";
+
+  /* Test Data for now */
   export let data;
+
+  if (!Array.isArray(data.options)) {
+    throw new Error("Options Menu: Data is not in an array format");
+  }
+
+  function loadCSSOptions(config) {
+    let root = document.documentElement;
+    let cssProperties = Object.entries(config);
+    for (const [key, value] of cssProperties) {
+      root.style.setProperty("--" + key, value.toString());
+    }
+  }
+
+  onMount(() => {
+    loadCSSOptions(data.css);
+  });
 </script>
 
 <div class="container">
   <div class="options-menu">
-    <div class="option">
-      <input class="slider" type="range" min="0" max="100" />
-    </div>
+    {#each data.options as option}
+      <div class="option">
+        <div class="input">
+          <input
+            type={option.type}
+            min={option.min}
+            max={option.max}
+            value={option.start}
+          />
+        </div>
+      </div>
+    {/each}
   </div>
 </div>
 
@@ -27,13 +55,16 @@
     width: 30vw;
     height: 94.6vh;
 
-    padding: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
   }
 
   .option {
     border-radius: 10px;
     background-color: #f3d3bd;
     border: solid 2px white;
-    height: 5vh;
+    height: 7vh;
+    margin-bottom: 1vh;
   }
 </style>
